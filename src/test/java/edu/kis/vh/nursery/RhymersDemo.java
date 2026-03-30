7f7ebadfe923f25d33d01f76d2e0a2167c690805
unknown <255738@helium.com>
2026-03-30 10:58:40 +0200

    13. uporzadkowanie aplikacji demo RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -12,27 +12,31 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
+
+        testRhymers(factory);
+
+    }
+
+    private static void testRhymers(Rhymersfactory factory) {
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
-        
+
         for (int i = 1; i < LAST_RHYMER_NUMBER; i++)
             for (int j = 0; j < RHYMERS_COUNT-1; j++)
                 rhymers[j].countIn(i);
-        
+
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < LAST_RHYMER_NUMBER; i++)
             rhymers[RHYMERS_COUNT-1].countIn(rn.nextInt(MAX_RND_NUMBER));
-        
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
         System.out.println(TOTAL_REJECTED_IS_TEXT
                 + ((HanoiRhymer) rhymers[RHYMERS_COUNT-1]).reportRejected());
-        
     }
-    
+
 }
